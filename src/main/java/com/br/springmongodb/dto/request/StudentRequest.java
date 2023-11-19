d290a5f6782e066febe8668fdcf293ddf91e7893
Alison8547 <alisonailson100@hotmail.com>
2023-11-19 17:34:23 -0300

    feat: dtos and mapper

---
+++ src/main/java/com/br/springmongodb/dto/request/StudentRequest.java
@@ -0,0 +1,38 @@
+package com.br.springmongodb.dto.request;
+
+import com.br.springmongodb.domain.School;
+import com.br.springmongodb.enums.Gender;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class StudentRequest {
+
+
+    @NotBlank
+    private String name;
+
+    @NotNull
+    private Integer age;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotNull
+    private Gender gender;
+
+    @NotBlank
+    private List<String> hobbies;
+
+    @NotNull
+    private School school;
+}
